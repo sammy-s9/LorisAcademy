7307b24c6ca20da45820983738a61e3dbe029f33
Sam Setayesh <sam_setayesh@hotmail.com>
2022-04-19 17:42:19 +0430

    Add search box

--- js/main.js
+++ js/main.js
@@ -64,3 +64,22 @@ function navbarColorChanger() {
     navbar[0].classList.remove('bg-dark');
   }
 }
+
+$(function () {
+  $("#search-menu").removeClass("toggled");
+
+  $("#search-icon").click(function (e) {
+    e.stopPropagation();
+    $("#search-menu").toggleClass("toggled");
+    $("#popup-search").focus();
+  });
+
+  $("#search-menu input").click(function (e) {
+    e.stopPropagation();
+  });
+
+  $("#search-menu, body").click(function () {
+    $("#search-menu").removeClass("toggled");
+  });
+});
+
